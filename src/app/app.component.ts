7f1433d888187986c16fe8028d233bcdb3d0f016
M3rzh1n <96054590+M3rzh1n@users.noreply.github.com>
2022-07-26 20:08:23 +0300

    original commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
+import { authCodeFlowConfig } from './sso.config'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OktaAngularApp';
+
+  constructor(private oauthService:OAuthService){
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn(){
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin().then(() => {
+
+      if (this.oauthService.hasValidAccessToken()) {
+
+        const accesstToken = this.oauthService.getAccessToken(); //Access Token
+        console.log(accesstToken);
+      }
+    }
+    )
+
+  }
+
+  login(){
+    this.oauthService.initImplicitFlow();
+
+  }
+
+  logout(){
+    this.oauthService.logOut();
+  }
+
+  get token(){
+    let claims:any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
 }
