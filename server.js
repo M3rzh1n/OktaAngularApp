35d89f0399e731ed5bb9ddeed70089785e50e40e
M3rzh1n <96054590+M3rzh1n@users.noreply.github.com>
2022-07-26 23:08:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/okta-angular-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/okta-angular-app/index.html'))
 );
 
 // Start the app by listening on the default Heroku port
